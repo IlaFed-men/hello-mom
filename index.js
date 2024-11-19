ba4955b566a0925626f810c027614f769d831c88
IlaFed-men <cetsfed7@gmail.com>
2024-11-19 22:29:22 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click',  function() {
+    output.innerHTML = 'Берегите природу!'
+});
+const Basic = document.querySelector('.Basic');
+
+button.addEventListener('click', function() {
+    Basic.innerHTML='Светлана и Илья'
+});
